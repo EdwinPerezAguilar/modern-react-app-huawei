3d628772be505d2dca9dca73b8f4818eb7afccf5
Edwin Perez Aguilar <e84236353@china.huawei.com>
2022-06-27 13:59:55 -0500

    swr-commit

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,7 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Hi I&apos;m Edwin PA a Solution Architect</h1>
+      <h1 className="gradient__text">Hi I&apos;m Edwin Perez Aguilar a Solution Architect</h1>
       <p>I am a disciplined person who has participated in electrical-electronics projects focused on embedded systems and serial communications. Applying my experience, knowlegde and skills commited to excellence, seeking to contribute to the succes of the projects where I participate and always learning from the feedback to develop my profesional growth.</p>
 
       <div className="gpt3__header-content__input">
